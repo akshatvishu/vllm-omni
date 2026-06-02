cae62ee77a6bfa7ea26a86c6afba21dd2acdc5dd
akshatvishu <akshatnayak197@gmail.com>
2026-06-02 23:26:46 +0530

    fix: support newer transformers 5.X in Ming Flash Omni talker
    Adapt the Ming Flash Omni talker compatibility fixes suggested in PR #4080.
    
    Suggested-by: Yuanheng Zhao <jonathan.zhaoyh@gmail.com>
    Signed-off-by: akshatvishu <akshatnayak197@gmail.com>

--- vllm_omni/model_executor/models/ming_flash_omni/talker_module.py
+++ vllm_omni/model_executor/models/ming_flash_omni/talker_module.py
@@ -807,7 +807,7 @@ class MingAudioGenerator:
                 use_cache=True,
             )
         else:
-            past_seen_tokens = past_key_values.get_seq_length()
+            past_seen_tokens = int(past_key_values.get_seq_length())
             cache_position = torch.arange(
                 past_seen_tokens,
                 past_seen_tokens + inputs_embeds.shape[1],
