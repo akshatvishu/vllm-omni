72941dcdf334d70fc8335ae4d71c8620a7eec822
akshatvishu <akshatnayak197@gmail.com>
2026-07-08 18:05:12 +0530

    [BugFix][Metrics] Fix stage metrics iteration_stats overwrite
    Signed-off-by: akshatvishu <akshatnayak197@gmail.com>

--- vllm_omni/engine/stage_pool.py
+++ vllm_omni/engine/stage_pool.py
@@ -1071,7 +1071,8 @@ class StagePool:
             return []
         client = cast(StagePoolLLMClient, raw_client)
         processor = self.output_processor
-        iteration_stats = IterationStats()
+        if iteration_stats is None:
+            iteration_stats = IterationStats()
         processed = processor.process_outputs(
             raw_outputs.outputs,
             raw_outputs.timestamp,
