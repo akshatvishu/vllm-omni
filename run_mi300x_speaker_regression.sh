eb1c181d32e24668931b62fed5f01a768a2fcbab
akshatvishu <akshatnayak197@gmail.com>
2026-07-13 18:25:43 +0530

    [Test] Move mi300x speaker regression test to tests/e2e
    Signed-off-by: akshatvishu <akshatnayak197@gmail.com>

--- run_mi300x_speaker_regression.sh
+++ run_mi300x_speaker_regression.sh
@@ -5,5 +5,5 @@ label="${1:?usage: $0 before-fix|after-fix}"
 log="$PWD/mi300x_speaker_regression_${label}.log"
 
 python -m pytest -sv \
-  "$PWD/mi300x_speaker_regression_test.py::test_ethan_survives_non_async_stage_handoff" \
+  "$PWD/tests/e2e/online_serving/test_mi300x_speaker_regression_local.py::test_ethan_survives_non_async_stage_handoff" \
   --run-level full_model 2>&1 | tee "$log"
