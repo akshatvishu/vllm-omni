59cdf11439fa283330d6d89b79c5e0cc1697a69f
akshatvishu <akshatnayak197@gmail.com>
2026-07-13 18:30:11 +0530

    [Test] Point speaker regression script to test_qwen3_omni_expansion.py
    Signed-off-by: akshatvishu <akshatnayak197@gmail.com>

--- run_mi300x_speaker_regression.sh
+++ run_mi300x_speaker_regression.sh
@@ -5,5 +5,5 @@ label="${1:?usage: $0 before-fix|after-fix}"
 log="$PWD/mi300x_speaker_regression_${label}.log"
 
 python -m pytest -sv \
-  "$PWD/tests/e2e/online_serving/test_mi300x_speaker_regression_local.py::test_ethan_survives_non_async_stage_handoff" \
+  "$PWD/tests/e2e/online_serving/test_qwen3_omni_expansion.py::test_speaker_002[default]" \
   --run-level full_model 2>&1 | tee "$log"
